89bc22296816c7174f14ea2c53e3eb0bc5812811
Serhii Yatsenko <serhii2111@yahoo.com>
2023-05-21 16:15:19 +0300

    01

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,4 +1,55 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
+// console.log(galleryItems);
 
-console.log(galleryItems);
+const galleryContainer = document.querySelector(".gallery");
+const galleryItemsMarkup = createGalleryItemsMarkup(galleryItems);
+
+galleryContainer.insertAdjacentHTML("beforeend", galleryItemsMarkup);
+
+galleryContainer.addEventListener("click", onGalleryItemClick);
+
+function createGalleryItemsMarkup(galleryItems) {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `<li class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</li>`;
+    })
+    .join("");
+}
+
+
+function onGalleryItemClick(evt) {
+     console.log(evt.target.dataset.source);
+
+   evt.preventDefault();
+  const isGallerySwatchItem = evt.target.classList.contains('.gallery__item');
+//   const isGallerySwatchItem = evt.target.classList.contains("img");
+
+  if (!isGallerySwatchItem) {
+    return;
+  }
+
+    // console.log(evt.target.dataset.source);
+const instance = basicLightbox.create
+    (`<img src= "${evt.target.dataset.source}" width="800" height="600">`,
+    {
+                onShow: () => {
+                    window.addEventListener('keydown', onEscKeyPress);
+                },
+                onClose: () => {
+                    window.removeEventListener('keydown', onEscKeyPress);
+                },
+            });
+  instance.show();
+
+//   console.log(evt.target.dataset.source);
+}
